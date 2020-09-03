8ed740aaf6a002d8595138856fd5beaf2af2289a
Yehong Piao <piaoyehong1107@gmail.com>
2020-09-03 04:09:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ var var1=2;
 var var2=31;
 var multiply= var1*var2;
 var random=Math.ceil(Math.random()*10);
+<<<<<<< HEAD
 var mod= 16;
 mod %= 6;
+=======
+var mod=16 %= 3;
+>>>>>>> 711bc064019dbbff1bed01bc49455c77428de92a
 
 var max=Math.max(1,2,20)
